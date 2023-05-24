bbf72e96195aefbe53ecd4c92a1048c656f9bdd5
rali11 <edu_2015@live.com>
2023-05-24 17:51:42 -0300

    feat(ui): se agrego componente Card

---
+++ src/components/quiz/Quiz.vue
@@ -0,0 +1,28 @@
+<template>
+  <Container tag="section">
+    <Card class="quiz">
+      <h1>Hola mundo</h1>
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates perspiciatis natus molestias ratione similique quaerat consequuntur reiciendis, at dignissimos vero vitae est enim, ipsam eveniet quasi, voluptas id? Rem, hic!</p>
+    </Card>
+  </Container>
+</template>
+
+<script setup>
+  import Card from '@/components/ui/atoms/Card.vue';
+  import Container from '@/components/ui/objects/Container.vue';
+  
+</script>
+<style lang="scss" scoped>
+  @use "@/assets/styles/settings/variables";
+  .container {
+    margin: auto;
+    display: flex;
+    justify-content: center;
+    color: black;
+    .quiz {
+      width: 29rem;
+      padding-top: 4.35rem;
+      padding-bottom: 2rem;
+    }
+  }
+</style>
