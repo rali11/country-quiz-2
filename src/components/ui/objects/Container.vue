f18fe65aefbc5cfa9a2c6b6118d6c85a85415066
rali11 <edu_2015@live.com>
2023-05-24 17:02:51 -0300

    feat(container): se agrego componente container

---
+++ src/components/ui/objects/Container.vue
@@ -0,0 +1,42 @@
+<template>
+  <component 
+    :is="tag" 
+    class="container"
+  >
+    <slot />
+  </component>
+</template>
+
+<script setup>
+  defineProps({
+    tag: {
+      type: String,
+      default:'div',
+      validator(tag){
+        return ['div','section'].includes(tag);
+      }
+    }
+  })
+</script>
+
+<style lang="scss" scoped>
+@use "@/assets/styles/settings/variables";
+  .container {
+    --container-width: #{variables.$mobile-xs};
+
+    width: var(--container-width);
+    margin:0 auto;
+
+    @media (min-width: variables.$mobile){
+      --container-width: #{variables.$mobile};
+    }
+
+    @media (min-width: variables.$tablet){
+      --container-width: #{variables.$tablet};
+    }
+
+    @media(min-width: variables.$desktop){
+      --container-width: #{variables.$desktop};
+    }
+  }
+</style>
