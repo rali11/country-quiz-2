f3f50eeb95db2f622856d8c27fa6b858093d937b
rali11 <edu_2015@live.com>
2023-07-31 01:30:27 -0300

    refactor(modelos): se agrego metodos a Quiz

--- src/models/Quiz/Quiz.js
+++ src/models/Quiz/Quiz.js
@@ -1,8 +1,13 @@
+import Choice from '@/models/Choice/Choice'
+
 export class Quiz {
   #choices
   #correctChoice
 
   constructor(choices){
+    choices.forEach(choice => {
+      if(!(choice instanceof Choice)) throw new Error('Should be extended from Choice class')
+    })
     this.#choices = choices
     this.#correctChoice = choices[0]
   }
@@ -11,11 +16,13 @@ export class Quiz {
     return ''
   }
 
-  getChoices(){
-    return this.#choices
+  validateChoice(selectedChoice){
+    if (!(selectedChoice instanceof Choice)) throw new Error('Should be extended from Choice class')
+    return this.#correctChoice.isEqualTo(selectedChoice)
   }
 
-  validateChoice(selectedChoice){
-    return this.#correctChoice.isEqualTo(selectedChoice)
+  isEqualTo(quiz){
+    if (!(quiz instanceof Quiz)) throw new Error('Should be extended from Quiz class') 
+    return quiz.validateChoice(this.#correctChoice)
   }
 }
