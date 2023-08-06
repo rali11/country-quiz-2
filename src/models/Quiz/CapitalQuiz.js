b29ae47a8724e32d926ef4c1fbd78c01fb492337
rali11 <edu_2015@live.com>
2023-08-06 17:02:13 -0300

    fix(models): fix access to CapitalQuiz's props

--- src/models/Quiz/CapitalQuiz.js
+++ src/models/Quiz/CapitalQuiz.js
@@ -1,14 +1,14 @@
 import { Quiz } from "./Quiz"
 
 export class CapitalQuiz extends Quiz{
-  #correctChoice
+  correctChoice
 
   constructor(choices){
     super(choices)
-    this.#correctChoice = choices[0]
+    this.correctChoice = choices[0]
   }
 
   getQuestion(){
-    return `${this.#correctChoice.getValue()} is the capital of`
+    return `${this.correctChoice.getValue()} is the capital of`
   }
 }
