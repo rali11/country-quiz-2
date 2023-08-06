7accfd6a646981f7c9c43d2795e306d3f8141b21
rali11 <edu_2015@live.com>
2023-08-06 16:45:38 -0300

    fix(services): error en funcion getCountries

--- src/services/RestCountries.js
+++ src/services/RestCountries.js
@@ -5,7 +5,7 @@ export const getCountries = async () => {
     const response = await fetch('https://restcountries.com/v3.1/all');
     if (!response.ok) throw new Error(response.statusText);
     const json = await response.json();
-    return json.filter(item => item.capital.length > 0).map(item => {
+    return json.filter(item => item.capital?.length > 0).map(item => {
       return new Country(item.name.common, item.flags.png, item.capital[0])
     });
   } catch (error) {
