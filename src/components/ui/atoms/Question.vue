fbfabf1608e988cb4555fd1b559d6e6d86aaeb63
rali11 <edu_2015@live.com>
2023-10-04 16:25:04 -0300

    feat(ui): add animated transition to Question.vue

--- src/components/ui/atoms/Question.vue
+++ src/components/ui/atoms/Question.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="question">
+  <div :class="['question', showClass]">
     <img class="question__flag-img" v-show="question.urlImg" :src="question.urlImg" alt="">
     <h3>{{ question.question }}</h3>
   </div>
@@ -7,17 +7,25 @@
 
 <script setup lang="ts">
   import type { QuestionInterface } from '@/models';
+  import { computed } from 'vue';
 
   interface Props {
     question: QuestionInterface
+    show: boolean
   }
 
-  defineProps<Props>()
+  const props = defineProps<Props>()
+
+  const showClass = computed(() => props.show ? 'question--show':'')
 </script>
 
 <style lang="scss" scoped>
   @use '@/assets/styles/settings/variables';
   .question {
+    opacity: 0;
+    transform: translateX(-10rem);
+    transition: all .5s ease;    
+
     &__flag-img {
       width: 5.25rem;
       height: 3.37rem;
@@ -25,9 +33,16 @@
       border-radius: 4px;  
       margin-bottom:1rem;
     }
+    
     h3 {
       color: variables.$text-primary-dark;
       margin-bottom: 2rem;
+    }
+
+    &--show {
+      transition: all .5s ease;
+      transform: translateX(0);
+      opacity: 1;
     }
   }
 </style>
