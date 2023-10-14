cafce4d4c72aa454c3d8425ed56e78bc6c31bd57
rali11 <edu_2015@live.com>
2023-10-14 16:12:42 -0300

    feat(ui): add footer devChallenges.io

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <Quiz />
+  <footer>
+    <p>created by <a href="https://github.com/rali11">rali11</a> - devChallenges.io</p>
+  </footer>
 </template>
 
 <script setup lang="ts">
@@ -8,9 +11,23 @@ import Quiz from './views/Quiz/Quiz.vue'
 </script>
 
 <style lang="scss">
+  @use "@/assets/styles/settings/variables"; 
+
   #app {
     height: 100vh;
     display: flex;
-    padding: 2rem 0;
+    flex-direction: column;
+    align-items: center;
+    padding: 0;
+  }
+
+  footer {
+    font-size: variables.$fs-footer;
+    font-weight: 400;
+    font-family: variables.$font-family-footer;
+
+    a {
+      font-weight: 600;
+    }
   }
 </style>
